e08b95be50149356379d668325907719bd3cc2b8
yunchan29 <113772243+yunchan29@users.noreply.github.com>
2023-11-03 00:56:29 +0800

    animation tweak

--- index.js
+++ index.js
@@ -14,3 +14,20 @@ document.body.addEventListener('scroll', (e) => {
     }
     }
 });
+
+document.body.addEventListener('scroll', (e) => {
+
+  var reveals=document.querySelectorAll('.reveal2');
+  for(var i=0; i< reveals.length;i++){
+    var windowheight=window.innerHeight;
+    var revealtop=reveals[i].getBoundingClientRect().top;
+    var revealpoint=150;
+
+    if(revealtop < windowheight - revealpoint){
+      reveals[i].classList.add('active');
+    }
+    else{
+      reveals[i].classList.remove('active');
+    }
+    }
+});
